78e5dac3b60c239f0b329370391e08c2d5dff55b
Andrey <vaa.andrey@gmail.com>
2016-04-13 10:59:20 +0600

    Version 1.3.6 small enhancement

--- src/kz/smpp/mysql/MyDBConnection.java
+++ src/kz/smpp/mysql/MyDBConnection.java
@@ -306,7 +306,7 @@ public class MyDBConnection {
 
     public List<client> getClientsFromContentType(int contentTypeCode, String date) {
         List<client> lct = new ArrayList<>();
-        String sql_string = "SELECT id_client, msisdn, update_date FROM client_content_type left join clients " +
+        String sql_string = "SELECT top 300 id_client, msisdn, update_date FROM client_content_type left join clients " +
                 "ON id_client=id WHERE clients.status= 0 AND id_content_type =" + contentTypeCode + " AND id_client NOT IN " +
                 "(SELECT id_client FROM sms_line_main WHERE id_content_type = " + contentTypeCode + " AND date_send = '" + date + "')";
         try {
